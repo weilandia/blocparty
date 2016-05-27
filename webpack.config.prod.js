4b868bf3a1fc95f66bf8252d95f25876ad00b6e0
Nick Weiland <nickweiland@gmail.com>
2016-05-27 14:03:24 -0600

    update webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
